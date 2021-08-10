7372327e82431917c6a4e10d08bf809e32c4d837
Anna Gabriel <yssgabriel@gmail.com>
2021-08-10 12:37:45 +0800

    Update display and add keyboard shortcut

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,29 +3,34 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-import 'jquery'
-import '@doabit/semantic-ui-sass'
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+import 'jquery';
+import '@doabit/semantic-ui-sass';
 
-
-export const scrollBottom = function() {
-  const section = $('div[data-scroll]')
-  if (section.length > 0) {
-    section.scrollTop(section[0].scrollHeight)
-  }
+export const scrollBottom = function () {
+	const section = $("div[data-scroll='auto']");
+	if (section.length > 0) {
+		section.scrollTop(section[0].scrollHeight);
+	}
+};
+const resetFormOnSubmit = function() {
+  const form = $("form[data-onsubmit='reset']");
+  form.on('ajax:success', function(e) {
+    e.target.reset();
+  })
 }
-
-$(document).on("turbolinks:load", () => {
-  $('.ui.dropdown').dropdown();
-  $('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade');
-  });
-  scrollBottom()
+$(document).on('turbolinks:load', () => {
+	$('.ui.dropdown').dropdown();
+	$('.message .close').on('click', function () {
+		$(this).closest('.message').transition('fade');
+	});
+	scrollBottom();
+  resetFormOnSubmit();
 });
