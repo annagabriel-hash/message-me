acd25980edef1b36b21944d822f4332f371cb304
Anna Gabriel <yssgabriel@gmail.com>
2021-08-10 12:56:03 +0800

    Update message scope

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
-    @messages = Message.all
+    @messages = Message.last_20_messages
     @message = Message.new
   end
   
