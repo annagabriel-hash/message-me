3ae7a229633318c78eb6b7af2c57d54fbfc404e2
Anna Gabriel <yssgabriel@gmail.com>
2021-08-10 16:34:03 +0800

    Update CORS origin url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://shielded-reaches-06332.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://shielded-reaches-06332.herokuapp.com', /http:\/\/shielded-reaches-06332.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://shielded-reaches-06332.herokuapp.com', /http:\/\/shielded-reaches-06332.herokuapp.com*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
