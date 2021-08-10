623f42c497039f71ca51503a19ba93dc86fedfc3
Anna Gabriel <yssgabriel@gmail.com>
2021-08-10 13:51:17 +0800

    Update prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://shielded-reaches-06332.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://shielded-reaches-06332.herokuapp.com', /http:\/\/shielded-reaches-06332.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
